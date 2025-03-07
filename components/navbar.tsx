e52e1445411b82c301d029b237a8f86383a82edc
Jeel Gajera <83470656+JeelGajera@users.noreply.github.com>
2025-03-07 10:56:58 +0530

    Update navbar.tsx

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -62,7 +62,7 @@ export function Navbar() {
           <div className="flex items-center justify-between sm:gap-2">
             <div className="flex ml-4 sm:ml-0">
               <Link
-                href="https://github.com/quanta-js/quanta"
+                href="https://github.com/quanta-js"
                 className={buttonVariants({
                   variant: "ghost",
                   size: "icon",
